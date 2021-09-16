f586cb6c17c60f89c634c24c2fb731d0bbe5088a
urmamafat <90857499+urmamafat@users.noreply.github.com>
2021-09-16 18:21:39 -0400

    Add files via upload

---
+++ Tricky_but_hard_python_coed.py
@@ -0,0 +1,12 @@
+#TRICKY BUT REAL EASY PYTHON CODE
+# PRINT 0 IF NUM IS DIVISIBLE BY 5 ELSE PRINT 1
+# THE CONDITION IS YOU ONLY HAVE THE KNOWLEDGE OF OPERATORS (+,-,*,%,**,//); PRINT STATEMENTS AND INPUT STATEMENTS
+
+# TAKING IN A RANDOM NUMBER NUM
+num = 19
+
+
+rem = num %5
+rem = rem + 4
+floor_division_result  = rem // 5
+print(floor_division_result)
